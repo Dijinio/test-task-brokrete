ec609db919ba1bbaa5d9c49da2c8e1b76e57f484
dijinio <dijinio@gmail.com>
2022-02-25 15:02:17 +0000

    added tile size increase functionality

--- src/Components/TileItem.js
+++ src/Components/TileItem.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 function TileItem({ tValue, handleGeneratedNumber, handleTileRemove }) {
   const { id, number } = tValue;
+  const [largeTile, setLargeTile] = useState(false);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -12,7 +13,10 @@ function TileItem({ tValue, handleGeneratedNumber, handleTileRemove }) {
   }, []);
 
   return (
-    <span className="tile-item">
+    <span
+      className={`tile-item ${largeTile && "large-tile"}`}
+      onClick={() => setLargeTile((prev) => !prev)}
+    >
       <span className="close-btn" onClick={() => handleTileRemove(id)}>
         X
       </span>
