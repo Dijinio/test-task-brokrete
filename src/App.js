0cfd7d1dbce442ce0c831136b393953fc64ef9be
dijinio <dijinio@gmail.com>
2022-02-25 14:12:26 +0000

    added simple styles and number gen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import TileItem from "./Components/TileItem";
-import TileAddBtn from "./Components/TileAddBtn";
 
 const initialValues = [
   { id: 1, number: 2 },
@@ -12,16 +11,25 @@ function App() {
   const [tileValues, setTileValues] = useState(initialValues);
   const [generatedNumbers, setGeneratedNumbers] = useState([]);
 
-  function handleGeneratedNumber(num) {}
+  function generateNumber() {
+    const randomNum = Math.ceil(Math.random() * 9);
+    const newNum = { id: Date.now(), number: randomNum };
+    setGeneratedNumbers((prev) => [...prev, randomNum]);
+    setTileValues((prev) => [...prev, newNum]);
+  }
 
+  console.log(generatedNumbers);
   return (
     <div className="app-container">
       <h1>Test Application</h1>
+      <p className="generated-numbers">{generatedNumbers.join("")}</p>
       <div className="tile-container">
         {tileValues.map((val) => (
-          <TileItem tValue={val.number} />
+          <TileItem tValue={val.number} key={val.id} />
         ))}
-        <TileAddBtn handleGeneratedNumber={handleGeneratedNumber} />
+        <span className="tile-item add-btn" onClick={() => generateNumber()}>
+          +
+        </span>
       </div>
     </div>
   );
--- src/Components/TileAddBtn.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TileAddBtn({ handleGeneratedNumber }) {
-  return <span className="tile-item">+</span>;
-}
-
-export default TileAddBtn;
